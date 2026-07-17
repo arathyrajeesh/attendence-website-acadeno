49ffcb32598e9ea67840c08ac8c190a51f0b66e6
arathyrajeesh <arathyrajeesh2@gmail.com>
2026-07-17 12:31:31 +0530

    Add reportlab dependency

--- attendence/attendence/settings.py
+++ attendence/attendence/settings.py
@@ -7,8 +7,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-change-me-in-production')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost 127.0.0.1').split()
-
+ALLOWED_HOSTS = [
+    "arathy135.pythonanywhere.com",
+    "localhost",
+    "127.0.0.1",
+]
 # ── Apps ───────────────────────────────────────────────────────────────────────
 INSTALLED_APPS = [
     'django.contrib.admin',
